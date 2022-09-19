63349fe0381eff133fd437293d3d71757481ce94
katescha8 <kate.schapo.5@gmail.com>
2022-09-19 20:40:18 +0300

    Добавьте файлы проекта.

---
+++ Class2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount();
+            account = BankAccount.Saving;
+            Console.WriteLine(account);   
+        }
+    }
+    enum BankAccount
+    {
+        Current,
+        Saving
+    }
+
+}
---
+++ Home1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Home2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
